a5ef8c39b37c0e576523eaee18c1cfa264b58d4b
Chris Parker <ctp7313@gmail.com>
2020-11-23 21:08:51 -0700

    using hashrouter

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter, Route, Switch } from "react-router-dom";
+import { HashRouter, Route, Switch } from "react-router-dom";
 import Nav from "./components/Nav";
 import Home from "./pages/Home";
 import Contact from "./pages/Contact";
@@ -8,14 +8,14 @@ import Portfolio from "./pages/Portfolio";
 
 function App() {
   return (
-    <BrowserRouter basename={process.env.PUBLIC_URL}>
+    <HashRouter basename={process.env.PUBLIC_URL}>
         <Nav />
         <Switch>
-          <Route exact path={process.env.PUBLIC_URL + '/'} component={Home} />
+          <Route exact path="/" component={Home} />
           <Route exact path="/contact" component={Contact} />
           <Route exact path="/portfolio" component={Portfolio} />
         </Switch>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
 
