2fac1bffa0fa4fddce92f105013a6b87f6407697
Chris Parker <ctp7313@gmail.com>
2020-11-23 18:50:20 -0700

    20201123 deployment

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
 import Nav from "./components/Nav";
 import Home from "./pages/Home";
 import Contact from "./pages/Contact";
@@ -8,18 +8,15 @@ import Portfolio from "./pages/Portfolio";
 
 function App() {
   return (
-    <Router basename={process.env.PUBLIC_URL}>
-      <div>
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
         <Nav />
         <Switch>
           <Route exact path="/" component={Home} />
           <Route exact path="/contact" component={Contact} />
           <Route exact path="/portfolio" component={Portfolio} />
         </Switch>
-      </div>
-    </Router>
+    </BrowserRouter>
   );
 }
-
 
 export default App;
